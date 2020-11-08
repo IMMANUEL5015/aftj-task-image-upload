191c4d90982247e471073d8d4117624ab499498a
IMMANUEL5015 <immanueldiai@gmail.com>
2020-11-08 13:45:08 +0100

    Configure Cloudinary and Multer for Image Upload

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 require('dotenv').config();
 const express = require('express');
+const cloudinary = require('cloudinary');
+const multer = require('multer');
+
 const app = express();
+
+cloudinary.config({
+    cloud_name: 'immanueldiai',
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const multerStorage = multer.diskStorage({
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + Date.now());
+    }
+});
 
 app.get('/', (req, res) => {
     res.status(200).json({
         status: 'Success',
         message: 'Your API is running smoothly.'
+    });
+});
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        status: 'Fail',
+        message: 'The resource you are looking for cannot be found!'
     });
 });
 
